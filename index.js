4cbea58acae013e289df8c7b2642a7e65c6e389e
github username <github email address>
2017-11-19 22:05:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'joe';
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer(){
+  return bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer(){
+ return bestCustomer = 'maybe bob';
+};
+
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer = 'jane';
+};
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'bob';
+  let favoriteCustomer = 'jack';
+}
